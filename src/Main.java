6911856da238de8e4e7a559454471ce04698ffa9
unknown <BT_4N2_11@BKTECH.LOCAL>
2024-02-07 13:13:29 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("-------------------------------");
+        System.out.println("What would you like to do?");
+        System.out.println("1. Calculate cost of one package");
+        System.out.println("2. Simulate packages");
+        System.out.println("3. How package costs are calculated");
+        System.out.println("4. exit");
+        Scanner
+
+
+
+
+    }
+}
